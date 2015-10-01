0acd46686c67aa544bb241f9d3cd432cb7249a6b
Björn Gustavsson <bjorn@erlang.org>
2015-10-01 15:41:01 +0200

    epp: Add the -error and -warning directives
    If one of several alternatives configurations are required for
    an Erlang module to compile, but none are available, it would
    be useful to give a nice error message. For example:
    
      -ifdef(CONFIG_A).
         %% Some code if A is true.
      -else.
      -ifdef(CONFIG_B).
        %% Some code if B is true.
      -else.
        -error("Neither CONFIG_A nor CONFIG_B are available").
      -endif.
      -endif.
    
    If neither CONFIG_A nor CONFIG_B are defined, the error message
    will be:
    
      module.erl:10: -error("Neither CONFIG_A nor CONFIG_B are available").
    
    That is basically the same behavior as for the #error directive in
    GCC.
    
    For symmetry with the -error directive, add the -warning
    directive to generate a compiler warning. For example:
    
      -ifdef(HAVE_COOL_FEATURE).
         %% Code if we have Cool Feature.
      -else.
         %% Inefficient fallback code.
         -warning("Using inefficient fallback").
      -endif.
    
    If HAVE_COOL_FEATURE is not defined, the warning message will
    be:
    
      module.erl:8: Warning: -warning("Using inefficient fallback").
    
    That is basically the same behavior as for the #warning directive
    in GCC.

--- lib/stdlib/src/epp.erl
+++ lib/stdlib/src/epp.erl
@@ -46,6 +46,8 @@
 -type tokens() :: [erl_scan:token()].
 -type used() :: {name(), argspec()}.
 
+-type warning_info() :: {erl_anno:location(), module(), term()}.
+
 -define(DEFAULT_ENCODING, utf8).
 
 %% Epp state record.
@@ -150,11 +152,13 @@ scan_erl_form(Epp) ->
     epp_request(Epp, scan_erl_form).
 
 -spec parse_erl_form(Epp) ->
-        {'ok', AbsForm} | {'eof', Line} | {error, ErrorInfo} when
+    {'ok', AbsForm} | {error, ErrorInfo} |
+    {'warning',WarningInfo} | {'eof',Line} when
       Epp :: epp_handle(),
       AbsForm :: erl_parse:abstract_form(),
       Line :: erl_anno:line(),
-      ErrorInfo :: erl_scan:error_info() | erl_parse:error_info().
+      ErrorInfo :: erl_scan:error_info() | erl_parse:error_info(),
+      WarningInfo :: warning_info().
 
 parse_erl_form(Epp) ->
     case epp_request(Epp, scan_erl_form) of
@@ -207,6 +211,10 @@ format_error({illegal,How,What}) ->
     io_lib:format("~s '-~s'", [How,What]);
 format_error({'NYI',What}) ->
     io_lib:format("not yet implemented '~s'", [What]);
+format_error({error,Term}) ->
+    io_lib:format("-error(~p).", [Term]);
+format_error({warning,Term}) ->
+    io_lib:format("-warning(~p).", [Term]);
 format_error(E) -> file:format_error(E).
 
 -spec parse_file(FileName, IncludePath, PredefMacros) ->
@@ -251,9 +259,11 @@ parse_file(Ifile, Options) ->
 
 -spec parse_file(Epp) -> [Form] when
       Epp :: epp_handle(),
-      Form :: erl_parse:abstract_form() | {'error', ErrorInfo} | {'eof',Line},
+      Form :: erl_parse:abstract_form() | {'error', ErrorInfo} |
+	      {'warning',WarningInfo} | {'eof',Line},
       Line :: erl_anno:line(),
-      ErrorInfo :: erl_scan:error_info() | erl_parse:error_info().
+      ErrorInfo :: erl_scan:error_info() | erl_parse:error_info(),
+      WarningInfo :: warning_info().
 
 parse_file(Epp) ->
     case parse_erl_form(Epp) of
@@ -274,6 +284,8 @@ parse_file(Epp) ->
 	    end;
 	{error,E} ->
 	    [{error,E}|parse_file(Epp)];
+	{warning,W} ->
+	    [{warning,W}|parse_file(Epp)];
 	{eof,Location} ->
 	    [{eof,erl_anno:new(Location)}]
     end.
@@ -751,6 +763,10 @@ scan_toks([{'-',_Lh},{atom,_Ld,define}=Define|Toks], From, St) ->
     scan_define(Toks, Define, From, St);
 scan_toks([{'-',_Lh},{atom,_Ld,undef}=Undef|Toks], From, St) ->
     scan_undef(Toks, Undef, From, St);
+scan_toks([{'-',_Lh},{atom,_Ld,error}=Error|Toks], From, St) ->
+    scan_err_warn(Toks, Error, From, St);
+scan_toks([{'-',_Lh},{atom,_Ld,warning}=Warn|Toks], From, St) ->
+    scan_err_warn(Toks, Warn, From, St);
 scan_toks([{'-',_Lh},{atom,_Li,include}=Inc|Toks], From, St) ->
     scan_include(Toks, Inc, From, St);
 scan_toks([{'-',_Lh},{atom,_Li,include_lib}=IncLib|Toks], From, St) ->
@@ -805,6 +821,24 @@ scan_extends([{atom,Ln,A}=ModAtom,{')',_Lr}|_Ts], Ms0) ->
     Ms = Ms0#{'BASE_MODULE':={none,[ModAtom]}},
     Ms#{'BASE_MODULE_STRING':={none,[{string,Ln,ModString}]}};
 scan_extends(_Ts, Ms) -> Ms.
+
+scan_err_warn([{'(',_}|_]=Toks0, {atom,_,Tag}=Token, From, St) ->
+    try expand_macros(Toks0, {St#epp.macs, St#epp.uses}) of
+	Toks when is_list(Toks) ->
+	    case erl_parse:parse_term(Toks) of
+		{ok,Term} ->
+		    epp_reply(From, {Tag,{loc(Token),epp,{Tag,Term}}});
+		{error,_} ->
+		    epp_reply(From, {error,{loc(Token),epp,{bad,Tag}}})
+	    end
+    catch
+	_:_ ->
+	    epp_reply(From, {error,{loc(Token),epp,{bad,Tag}}})
+    end,
+    wait_req_scan(St);
+scan_err_warn(_Toks, {atom,_,Tag}=Token, From, St) ->
+    epp_reply(From, {error,{loc(Token),epp,{bad,Tag}}}),
+    wait_req_scan(St).
 
 %% scan_define(Tokens, DefineToken, From, EppState)
 
