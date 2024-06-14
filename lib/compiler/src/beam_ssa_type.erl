407a082931ef32b9e59ad8f16b44c2da1057c2b7
Björn Gustavsson <bjorn@erlang.org>
2024-06-14 03:58:17 +0200

    wip

--- lib/compiler/src/beam_ssa_type.erl
+++ lib/compiler/src/beam_ssa_type.erl
@@ -1699,6 +1699,17 @@ simplify_remote_call(maps=Mod, put=Name, [Key,Val,Map], Ts, I) ->
         _ ->
             simplify_pure_call(Mod, Name, [Key,Val,Map], I)
     end;
+simplify_remote_call(maps=Mod, update=Name, [Key,Val,Map], Ts, I) ->
+    case concrete_type(Map, Ts) of
+        #t_map{} ->
+            %% This call to maps:update/3 cannot fail because of an
+            %% invalid map. Replace with the slightly more efficient
+            %% `put_map` instruction.
+            Args = [#b_literal{val=exact},Map,Key,Val],
+            I#b_set{op=put_map,args=Args};
+        _ ->
+            simplify_pure_call(Mod, Name, [Key,Val,Map], I)
+    end;
 simplify_remote_call(Mod, Name, Args, _Ts, I) ->
     case erl_bifs:is_pure(Mod, Name, length(Args)) of
         true ->
