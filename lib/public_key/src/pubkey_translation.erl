2efd8bb320278ac01b6ecac822982ddd9bd424b9
Björn Gustavsson <bgustavsson@gmail.com>
2025-04-15 09:26:17 +0200

    wip

---
+++ lib/public_key/src/pubkey_translation.erl
@@ -0,0 +1,68 @@
+%%
+%% %CopyrightBegin%
+%%
+%% SPDX-License-Identifier: Apache-2.0
+%%
+%% Copyright Ericsson AB 2008-2025. All Rights Reserved.
+%%
+%% Licensed under the Apache License, Version 2.0 (the "License");
+%% you may not use this file except in compliance with the License.
+%% You may obtain a copy of the License at
+%%
+%%     http://www.apache.org/licenses/LICENSE-2.0
+%%
+%% Unless required by applicable law or agreed to in writing, software
+%% distributed under the License is distributed on an "AS IS" BASIS,
+%% WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+%% See the License for the specific language governing permissions and
+%% limitations under the License.
+%%
+%% %CopyrightEnd%
+%%
+
+-module(pubkey_translation).
+-moduledoc false.
+
+-export([decode/1,encode/1]).
+
+-define(_PKCS_FRAME_HRL_, true).
+-include("public_key_internal.hrl").
+
+-record('AttributeTypeAndValue',
+        {
+         type,   % id_attributes()
+         value   % term()
+        }).
+
+decode(#'SubjectPublicKeyInfo'{algorithm=AlgId0,subjectPublicKey=Key}) ->
+    #'SubjectPublicKeyInfo_algorithm'{algorithm=AlgId1,parameters=Params1} = AlgId0,
+    AlgId = decode(AlgId1),
+    Params = decode(Params1),
+    {'SubjectPublicKeyInfo', {'AlgorithmIdentifier', AlgId, Params}, Key};
+decode(#'DSA-Params'{p=P,q=Q,g=G}) ->
+    #'Dss-Parms'{p=P,q=Q,g=G};
+decode(#'SingleAttribute'{type=T,value=V}) ->
+    #'AttributeTypeAndValue'{type=T,value=V};
+decode(Tuple) when is_tuple(Tuple) ->
+    list_to_tuple(decode_list(tuple_to_list(Tuple)));
+decode(List) when is_list(List) ->
+    decode_list(List);
+decode(Other) ->
+    Other.
+
+decode_list(List) ->
+    [decode(E) || E <- List].
+
+encode(#'AttributeTypeAndValue'{type=T,value=V}) ->
+    #'SingleAttribute'{type=T,value=V};
+encode(#'Dss-Parms'{p=P,q=Q,g=G}) ->
+    #'DSA-Params'{p=P,q=Q,g=G};
+encode(Tuple) when is_tuple(Tuple) ->
+    list_to_tuple(encode_list(tuple_to_list(Tuple)));
+encode(List) when is_list(List) ->
+    encode_list(List);
+encode(Other) ->
+    Other.
+
+encode_list(List) ->
+    [encode(E) || E <- List].
