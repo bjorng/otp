b1e104369512b90eb5388d69092e6568056c7d31
Björn Gustavsson <bgustavsson@gmail.com>
2025-04-22 09:16:34 +0200

    Make pubkey_policy_tree compile again

--- lib/public_key/src/pubkey_policy_tree.erl
+++ lib/public_key/src/pubkey_policy_tree.erl
@@ -22,20 +22,10 @@
 -module(pubkey_policy_tree).
 -moduledoc false.
 
-%% -include("CryptographicMessageSyntax-2009.hrl").
-%% -include("DSS.hrl").
-%%-include("OCSP-2009.hrl").
-%% -include("PKCS-1.hrl").
-%% -include("PKCS-10.hrl").
-%% -include("PKCS-3.hrl").
-%% -include("PKCS-FRAME.hrl").
-%% -include("PKIX1-PSS-OAEP-Algorithms-2009.hrl").
-%% -include("PKIX1Explicit-2009.hrl").
-%%-include("PKIX1Implicit-2009.hrl").
-%% -include("PKIXAlgs-2009.hrl").
+-define(_PKCS_FRAME_HRL_, true).
+-include("public_key_internal.hrl").
 
--include("OTP-PUB-KEY.hrl").
--include("pubkey_defs.hrl").
+-define('anyPolicy', {2,5,29,32,0}).
 
 %% API
 -export([add_leaves/2,
