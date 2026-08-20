9c5b286bfab181621b15ff42adeb14e05e58c238
Björn Gustavsson <bjorn@erlang.org>
2026-08-20 13:10:03 +0200

    fixup! WIP: Add structs for non-local native record handling

--- erts/emulator/beam/erl_rename_me.h
+++ erts/emulator/beam/erl_rename_me.h
@@ -1,24 +1,33 @@
 typedef struct {
-    Eterm module;
-    Eterm name;
-    enum {
-        create,
-        update,
-        get_elements
-    } code_type;
+    Eterm module;               /* Key */
+    Eterm name;                 /* Key */
 
-    Eterm states[ERTS_ADDRESSV_SIZE];
+    ErtsExtModRec states[ERTS_ADDRESSV_SIZE];
+} ErtsExtRecord;
 
-    /* FieldName0 Dst0 FieldName1 Dst1 ... FieldNameN DstN */
-    Eterm names_and_dest[];
-} ErtsExtRecordKey;
+typedef ErtsExtModRec {
+    /* Literal-tagged CONS pointer if the module is loaded. The head
+     * of the CONS holds to pointer to the canonical
+     * ErtsRecordDefinition for each code generation, while the tail
+     * holds the default values.
+     *
+     * THE_NON_VALUE if the module is not loaded.
+     */
+    Eterm cons;
+
+    Hash rec_ops;               /* Hash table of ErtExtRecOp */
+};
 
 typedef struct {
-    bool is_loaded;
-
-    /* Literal-tagged CONS pointer. The head of the CONS holds to
-     * pointer to the canonical ErtsRecordDefinition for each code
-     * generation, while the tail holds the default values. */
-    Eterm cons;
     Eterm code;
-} ErtsExtRecordState;
+
+    enum {
+        create,                 /* Uses cons for creation. */
+        update,                 /* Only compares cons. */
+        get_elements            /* Only compares cons. */
+    } code_type;                /* Key */
+    
+    /* FieldName0 Dst0 FieldName1 Dst1 ... FieldNameN DstN;
+     * sorted in atom order of field names. */
+    Eterm names_and_dest[];     /* Key */
+} ErtsExtRecOp;
