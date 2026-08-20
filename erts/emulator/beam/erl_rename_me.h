ea8334515485e48e49e539110f8dd56518ec3f85
Björn Gustavsson <bjorn@erlang.org>
2026-08-20 13:10:03 +0200

    fixup! WIP: Add structs for non-local native record handling

--- erts/emulator/beam/erl_rename_me.h
+++ erts/emulator/beam/erl_rename_me.h
@@ -1,3 +1,29 @@
+/*
+ * %CopyrightBegin%
+ *
+ * SPDX-License-Identifier: Apache-2.0
+ *
+ * Copyright Ericsson AB 2026. All Rights Reserved.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ *
+ * %CopyrightEnd%
+ */
+
+#ifndef __ERL_RECORD_H__
+#define __ERL_RECORD_H__
+
+#include "sys.h"
 typedef struct {
     Eterm module;               /* Key */
     Eterm name;                 /* Key */
@@ -20,6 +46,7 @@ typedef ErtsExtModRec {
 
 typedef struct {
     Eterm code;
+    Uint64 ref_cnt;
 
     enum {
         create,                 /* Uses cons for creation. */
@@ -31,3 +58,5 @@ typedef struct {
      * sorted in atom order of field names. */
     Eterm names_and_dest[];     /* Key */
 } ErtsExtRecOp;
+
+#endif
